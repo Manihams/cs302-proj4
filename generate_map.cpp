11fda4b3c4a84529abb399d7191ea6123e013ae6
Manihams Suraparaju <msurapar@hydra28.eecs.utk.edu>
2025-04-05 15:40:54 -0400

    tar file

--- generate_map.cpp
+++ generate_map.cpp
@@ -1,3 +1,8 @@
+/* Lab Name: Path Finding
+ * Names: Pravin Adithya Srinivasan, Manihams Suraparaju
+ * Resources: Lecture notes, youtube video, geeksforgeeks, github
+ */
+
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -11,25 +16,26 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int N = stoi(argv[1]);
+	// map size (n*n)
+    int mapSize = stoi(argv[1]);
     srand(time(0));
 
-    // Define tile types and costs
-    vector<pair<char, int>> tiles = {
+    // Initialized the tile types and costs
+    vector<pair<char, int>> tileCost = {
         {'f', 3}, {'g', 1}, {'G', 2},
         {'h', 4}, {'m', 7}, {'r', 5}
     };
 
-    // Output tile definitions
+    // Outputs each map and its cost
     cout << tiles.size() << endl;
     for (auto& t : tiles) {
         cout << t.first << " " << t.second << endl;
     }
 
-    // Output map dimensions
+    // Output map dimensions getting rows and columns
     cout << N << " " << N << endl;
 
-    // Generate and output map
+    // Calculates and prints the map grid with random tiles
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             int idx = rand() % tiles.size();
@@ -39,7 +45,7 @@ int main(int argc, char* argv[]) {
         cout << endl;
     }
 
-    // Output start (0, 0) and end (N-1, N-1)
+    // Output start and end positions from top-left to bottom right
     cout << "0 0" << endl;
     cout << N-1 << " " << N-1 << endl;
 
